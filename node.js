4a45086cffa5313d8f8c0c7c20370bb96b0aa02d
Gustavo Silva <gutog2105@gmail.com>
2024-12-19 22:17:43 -0300

    fix

--- node.js
+++ node.js
@@ -1,28 +1,40 @@
-module.exports = {
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"prettier",
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	plugins: ["@typescript-eslint", "import-helpers"],
-	rules: {
-		eqeqeq: "error",
-		"no-unused-vars": "error",
-		"no-unreachable": "error",
-		"prefer-const": "error",
-		"no-console": ["error", { allow: ["warn", "error"] }],
-		"@typescript-eslint/explicit-function-return-type": "error",
-		"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-	},
-	settings: {
-		"import/parsers": {
-			[require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+const eslintRecommended = require("eslint/configs/recommended.json");
+const typescriptRecommended = require("@typescript-eslint/eslint-plugin/dist/configs/recommended.json");
+const prettierConfig = require("eslint-config-prettier");
+const typescriptParser = require("@typescript-eslint/parser");
+
+module.exports = [
+	{
+		parser: typescriptParser,
+		parserOptions: {
+			ecmaVersion: "latest",
+			sourceType: "module",
 		},
+		plugins: ["@typescript-eslint", "import-helpers"],
+		rules: {
+			...eslintRecommended.rules,
+			...typescriptRecommended.rules,
+			...prettierConfig.rules,
+			eqeqeq: "error",
+			"no-unused-vars": "error",
+			"no-unreachable": "error",
+			"prefer-const": "error",
+			"no-console": ["error", { allow: ["warn", "error"] }],
+			"@typescript-eslint/explicit-function-return-type": "error",
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{ argsIgnorePattern: "^_" },
+			],
+		},
+		settings: {
+			"import/parsers": {
+				[require.resolve("@typescript-eslint/parser")]: [
+					".ts",
+					".tsx",
+					".d.ts",
+				],
+			},
+		},
+		ignorePatterns: ["node_modules"],
 	},
-	ignorePatterns: ["node_modules"],
-};
+];
